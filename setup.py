e868d027dc20c9d426607785956d2413fa44f9d2
Le Phuong Nam <phuongnam0907@gmail.com>
2021-07-21 01:39:13 +0700

    Custom directory + setup environment
    Signed-off-by: Le Phuong Nam <phuongnam0907@gmail.com>

--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@ from os import listdir
 from os.path import isfile, join
 import os
 
-# print(os.path.basename("python_libs\libs"))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+current_dir = os.path.basename(dir_path)
 
-mypath = os.path.basename("python_libs\libs")
-onlyfiles = [(mypath + '/' +f) for f in listdir(mypath) if isfile(join(mypath, f))]
+packpath = os.path.basename(str(current_dir) + "\libs")
+packages = [(packpath + '/' + f) for f in listdir(packpath) if isfile(join(packpath, f))]
+
+libpath = os.path.basename(str(current_dir) + "\envLibs")
+libs = [(libpath + '/' + f) for f in listdir(libpath) if isfile(join(libpath, f))]
+
+print("Setting environment...")
+print("==================================")
+main(['install', '--no-index', '--find-links="' + current_dir + '\envLibs"'] + libs)
 
 print("Installing packages...")
 print("==================================")
-main(['install', '--no-index', '--find-links="python_libs\libs"'] + onlyfiles)
+main(['install', '--no-index', '--find-links="' + current_dir + '\libs"'] + packages)
 print("==================================")
 print("Successfully installed packages!!!")
