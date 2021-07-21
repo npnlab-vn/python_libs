f5b832886f1bf3993a7da1d8e6d5880ada0ef36d
Le Phuong Nam <phuongnam0907@gmail.com>
2021-07-22 04:36:38 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+import subprocess
+
+print("Library is developing...)
+
+'''
+proc = subprocess.Popen([sys.executable, "processing.py"], stderr=subprocess.PIPE)
+result,err = proc.communicate()
+exit_code = proc.wait()
+
+print("==================================")
+if str(err).find("ERROR:") > -1 or exit_code == 1:
+    print("Failed to install packages.")
+else:
+    print("Successfully installed packages!!!")
+
+'''
